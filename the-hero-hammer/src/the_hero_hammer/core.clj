d4e4f6c653552dfa1366839ac7ebd46837d2bfc5
David <david@templatious.org>
2016-01-16 20:23:39 +0200

    nay

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -3,8 +3,24 @@
             [org.httpkit.server :refer [run-server]])
   (:use hiccup.core))
 
-(defn index []
+(defn index
+  "meow"
+  []
   (html [:h1 "Dazlow!"]))
+
+(defn questions []
+  (["Do you like, have a beard?" "yes" "nay"]))
+
+(defn render-question [q]
+  (html [:form
+         [:p (nth q 0)]
+         (map #(1) (drop 1 q))
+         ]))
+
+(defn compute-unit [func keyindb]
+  {:processed 0 :count 0})
+
+(render-question (first (questions)))
 
 (defroutes myapp
   (GET "/" [] (index)))
