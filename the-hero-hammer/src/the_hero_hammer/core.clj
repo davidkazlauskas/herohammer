60ce9a558cc7878cbbb76aa82497a0e4dc7bba46
David <david@templatious.org>
2016-01-16 18:10:37 +0200

    hiccup

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -1,9 +1,14 @@
 (ns the-hero-hammer.core
   (:require [compojure.core :refer :all]
-            [org.httpkit.server :refer [run-server]]))
+            [org.httpkit.server :refer [run-server]])
+  (:use hiccup.core))
+
+(defn index []
+  (html [:h1 "Dazlow!"]))
 
 (defroutes myapp
-  (GET "/" [] "Hello World"))
+  (GET "/" [] (index)))
 
 (defn -main []
+  (println "Muah runnin!")
   (run-server myapp {:port 5000}))
