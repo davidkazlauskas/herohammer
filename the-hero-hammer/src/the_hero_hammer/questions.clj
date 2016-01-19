7c656ec075063d2b042560a5eea6d2da80bdeb40
David <david@templatious.org>
2016-01-19 19:59:25 +0200

    moar question

--- the-hero-hammer/src/the_hero_hammer/questions.clj
+++ the-hero-hammer/src/the_hero_hammer/questions.clj
@@ -5,3 +5,10 @@
     :shortname shortname
     :question question
     :options (into [] options)})
+
+(defn questions [& args]
+  (into [] (map-indexed
+     #(apply question %1 %2) args)))
+
+(defmacro questions-m [& args]
+  (apply questions args))
--- the-hero-hammer/test/the_hero_hammer/questions_test.clj
+++ the-hero-hammer/test/the_hero_hammer/questions_test.clj
@@ -5,7 +5,17 @@
 
 (deftest question-tests
   (testing "Generate expected"
-  (is (= (question 0 "some question" "yes" "no")
-         {:id 0 :shortname "some question"
-          :question "yes" :options ["no"]}
-         ))))
+  (is (= (question 0 "sname" "some question" "yes" "no")
+         {:id 0 :shortname "sname"
+          :question "some question" :options ["yes" "no"]}
+         ))
+  (is (= (questions-m ("shorta" "some q a" "yes" "no")
+                     ("shortb" "some another" "1" "0"))
+         [
+          {:id 0 :shortname "shorta" :question "some q a"
+           :options ["yes" "no"]}
+          {:id 1 :shortname "shortb" :question "some another"
+           :options ["1" "0"]}
+         ]
+         ))
+  ))
