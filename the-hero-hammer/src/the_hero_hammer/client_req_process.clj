1001ca66ad0047638d05473c82c9b08b13094b1e
David <david@templatious.org>
2016-01-19 21:15:45 +0200

    prepping

--- the-hero-hammer/src/the_hero_hammer/client_req_process.clj
+++ the-hero-hammer/src/the_hero_hammer/client_req_process.clj
@@ -1,4 +1,15 @@
 (ns the-hero-hammer.client_req_process
   (:require [the-hero-hammer.questions :refer :all]
+            [the-hero-hammer.questions_spec :refer :all]
             [the-hero-hammer.storage :refer :all]))
+
+
+(def ^:dynamic *global-questions-map-lol* (all-questions-lol))
+(def ^:dynamic *global-short-to-index-map*
+  (->> *global-questions-map-lol*
+       (map-indexed #(vector (get %2 :shortname) %1))
+       (into {})))
+
+(defn store-question [arg-map]
+  ())
 
