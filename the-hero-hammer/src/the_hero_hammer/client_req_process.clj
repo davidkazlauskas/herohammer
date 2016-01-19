4a9dbd1b8764a47ec4ceac31043b0bfdc11b2d40
David <david@templatious.org>
2016-01-19 21:39:35 +0200

    try store questions

--- the-hero-hammer/src/the_hero_hammer/client_req_process.clj
+++ the-hero-hammer/src/the_hero_hammer/client_req_process.clj
@@ -10,6 +10,29 @@
        (map-indexed #(vector (get %2 :shortname) %1))
        (into {})))
 
+(defn gen-key-for-count-lol
+  [hero-user hero-opponent question-id filter-id]
+  (str "lol-question-count-" hero-user
+       "-" hero-opponent "-" question-id "-" filter-id))
+
+(defmacro lol-question-count-key [] "lol-total-question-counter")
+
+(defn lol-question-by-id-key [id]
+  (str "lol-question-by-id-" id))
+
+(defn lol-store-next-question [data]
+  (let [curr (get-key (lol-question-count-key))]
+    (let [toreturn
+          (if (some? curr)
+          (do (set-key (lol-question-count-key) (inc curr))
+              curr)
+          (do (set-key (lol-question-count-key) 1)
+              0))]
+      (set-key (lol-question-by-id-key toreturn) data)
+      toreturn)
+    )
+  )
+
 (defn store-question [arg-map]
   ())
 
