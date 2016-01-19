35390bd5964b38447c98567c952a7ac075bc842b
David <david@templatious.org>
2016-01-19 20:46:57 +0200

    some data

--- the-hero-hammer/src/the_hero_hammer/questions_spec.clj
+++ the-hero-hammer/src/the_hero_hammer/questions_spec.clj
@@ -8,4 +8,12 @@
     ("ladder" "What is your ranking?"
               "Bronze" "Silver" "Gold" "Platinum"
               "Diamond" "Master" "Challenger")
+    ("poking" "I got poked a lot" "yes" "no")
+    ("poking-o" "I poked my opponent a lot" "yes" "no")
+    ("sustain" "I could not sustain well" "yes" "no")
+    ("sustain-o" "My opponent could not sustain me" "yes" "no")
+    ("first-blood" "I got first blood" "yes" "no")
+    ("cc" "I got a lot of cc (stuns/slows)" "yes" "no")
+    ("gank" "I was ganked quite often" "yes" "no")
+    ("last-hit" "I had trouble last hitting minions" "yes" "no")
     ))
