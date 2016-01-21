b3d248273d8029c4ed61f3afb8cc525b847e21e3
David <david@templatious.org>
2016-01-21 17:45:24 +0200

    tests

--- the-hero-hammer/test/the_hero_hammer/core_test.clj
+++ the-hero-hammer/test/the_hero_hammer/core_test.clj
@@ -5,3 +5,15 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(defn test-form []
+  {"user-hero" 7 "opponent-hero" 11 "gank" 1 "last-hit" 0 "user-comment" "meow"})
+
+(deftest form-test
+  (testing "form processing test"
+    (is (= (form-to-data (test-form))
+           {:hero-user 7
+            :hero-opponent 11
+            :comment "meow"
+            :answers [[8 1] [9 0]]}))
+    ))
