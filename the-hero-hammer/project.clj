fa63bf7e47e4b1fa1aee4b356f81405616e806a1
David <david@templatious.org>
2016-01-21 18:32:50 +0200

    nippy

--- the-hero-hammer/project.clj
+++ the-hero-hammer/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]
+                 [com.taoensso/nippy "2.10.0"]
                  [compojure "1.1.8"]
                  [http-kit "2.1.18"]]
   :main the-hero-hammer.core)
