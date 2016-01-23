33a5828fbc119c349527df2666fea6c3e924be10
David <david@templatious.org>
2016-01-23 09:32:34 +0200

    much stuff

--- the-hero-hammer/src/the_hero_hammer/processing_job.clj
+++ the-hero-hammer/src/the_hero_hammer/processing_job.clj
@@ -1,2 +1,20 @@
 (ns the-hero-hammer.processing_job
-  (:require [the-hero-hammer.client_req_process :refer :all]))
+  (:require [the-hero-hammer.client_req_process :refer :all]
+            [the-hero-hammer.storage :refer :all]
+            ))
+
+(defn gen-key-for-filter-matchup-id
+  [hero-user-id hero-opponent-id filter-id]
+  (str "lol-filter-for-matchup-"
+       hero-user-id "-" hero-opponent-id "-" filter-id))
+
+(defn main-filter []
+  {:fetch-record []})
+
+(defmacro lol-new-unprocessed-questions-key
+  [idx]
+  (str "lol-processed-questions-for-filter-" idx))
+
+(defn lol-get-unproccessed-questions []
+  (let [outdb] (get-key (lol-new-unprocessed-questions-key)))
+)
