8c863353b7bd8be9e8124f636b27978288a62860
David <david@templatious.org>
2016-01-23 09:49:35 +0200

    ranging

--- the-hero-hammer/src/the_hero_hammer/processing_job.clj
+++ the-hero-hammer/src/the_hero_hammer/processing_job.clj
@@ -8,8 +8,26 @@
   (str "lol-filter-for-matchup-"
        hero-user-id "-" hero-opponent-id "-" filter-id))
 
+(defn fetch-filter-count
+  [hero-user-id hero-opponent-id filter-id]
+  (get-key (gen-key-for-filter-matchup-id
+             hero-user-id hero-opponent-id filter-id)))
+
+(defmacro count-in [] 1)
+(defmacro ignore [] 2)
+(defmacro drop-out [] 2)
+
+(defn max-available-range [curr-max my-range]
+  (cond (<= (:to my-range) curr-max)
+    {:from (:to my-range) :to curr-max}
+    :else (:from (:to my-range) :to (:to my-range))))
+
 (defn main-filter []
-  {:fetch-record []})
+  {:id 0
+   :record-key gen-key-for-filter-matchup-id
+   :expected (fn [curr-max record] "")
+   :process-question [curr-id record]
+   })
 
 (defmacro lol-new-unprocessed-questions-key
   [idx]
