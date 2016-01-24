2c982b2ff12644f6655e19c741daa74af954a028
David <david@templatious.org>
2016-01-24 08:11:30 +0200

    some stuff

--- the-hero-hammer/src/the_hero_hammer/hh_context.clj
+++ the-hero-hammer/src/the_hero_hammer/hh_context.clj
@@ -3,9 +3,11 @@
 (def ^:dynamic *hh-context* nil)
 
 (defn get-key [& args]
+  "Get key in db"
   (apply (:get-key (:dbinfo *hh-context*)) args))
 
 (defn set-key [& args]
+  "Set key in db"
   (apply (:set-key (:dbinfo *hh-context*)) args))
 
 (defn set-if-not-exists
@@ -13,3 +15,29 @@
   [db-key value]
   (if (nil? (get-key db-key))
     (set-key db-key value)))
+
+(defn all-filters []
+  ((:filters *hh-context*))
+  )
+
+
+
+
+; FILTER FUNCS
+(defmacro count-in [] 1)
+(defmacro ignore [] 2)
+(defmacro drop-out [] -1)
+
+(defn max-available-range [curr-max my-range]
+  (cond (<= (:to my-range) curr-max)
+    {:from (:to my-range) :to curr-max}
+    :else {:from (:to my-range) :to (:to my-range)}))
+
+(defn ahead-of-range [number the-range]
+  (if (< (:to the-range) number) true nil))
+
+(defn in-range [number the-range]
+  (if (and
+        (> (:to the-range) number)
+        (<= (:from the-range) number)) true nil))
+
