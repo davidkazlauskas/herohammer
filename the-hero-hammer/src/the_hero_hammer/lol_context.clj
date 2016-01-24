d387f5c5a2914c2b2e17c899ab2c9e121d318196
David <david@templatious.org>
2016-01-24 08:11:10 +0200

    dropped some

--- the-hero-hammer/src/the_hero_hammer/lol_context.clj
+++ the-hero-hammer/src/the_hero_hammer/lol_context.clj
@@ -1,8 +1,22 @@
 (ns the-hero-hammer.lol_context
   (:require [the-hero-hammer.questions :refer :all]
             [the-hero-hammer.questions_spec :refer :all]
-            [the-hero-hammer.storage :refer :all]
+            [the-hero-hammer.hh_context :refer :all]
             [taoensso.nippy :as nippy]))
+
+(defn main-filter []
+  {:id 0
+   :expected max-available-range
+   :process-question (fn [curr-id rec]
+     (cond
+       (in-range curr-id rec) (ignore)
+       (ahead-of-range curr-id rec) (drop-out)
+       (end-of-range curr-id rec) (count-in)))
+   :required-questions ["poking"]
+   })
+
+(defn all-filters []
+  [(main-filter)])
 
 (def ^:dynamic *hh-context*
   {
@@ -11,4 +25,6 @@
      :set-key the-hero-hammer.storage/set-key
      :set-if-not-exists the-hero-hammer.storage/set-if-not-exists
    }
+   :filters (all-filters)
   })
+
