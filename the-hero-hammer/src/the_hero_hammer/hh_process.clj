9a84a581433e770717cc43c432a75b5fdc61d3d8
David <david@templatious.org>
2016-01-24 12:09:45 +0200

    glob q count

--- the-hero-hammer/src/the_hero_hammer/hh_process.clj
+++ the-hero-hammer/src/the_hero_hammer/hh_process.clj
@@ -1,3 +1,11 @@
 (ns the-hero-hammer.hh_process
   (:require [the-hero-hammer.hh_context :refer :all]))
 
+(defn global-question-count []
+  (get-key ((fn-global-question-count))))
+
+(defn lol-process-pairs [to-process]
+  (let [currmax (global-question-count)]
+    (into [] (map
+       (partial lol-process-single-pair currmax)
+       to-process))))
