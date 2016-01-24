b8945f850531fffa1097e9aa20e101996735e712
David <david@templatious.org>
2016-01-24 08:05:29 +0200

    puttin in

--- the-hero-hammer/src/the_hero_hammer/hh_context.clj
+++ the-hero-hammer/src/the_hero_hammer/hh_context.clj
@@ -1,7 +1,15 @@
-(ns the-hero-hammer.hh_context
-  (:require [taoensso.nippy :as nippy]))
+(ns the-hero-hammer.hh_context)
 
 (def ^:dynamic *hh-context* nil)
 
 (defn get-key [& args]
-  ((:get-key (:dbinfo *hh-context*)) args))
+  (apply (:get-key (:dbinfo *hh-context*)) args))
+
+(defn set-key [& args]
+  (apply (:set-key (:dbinfo *hh-context*)) args))
+
+(defn set-if-not-exists
+  "Put data into storage if it doesn't exist"
+  [db-key value]
+  (if (nil? (get-key db-key))
+    (set-key db-key value)))
