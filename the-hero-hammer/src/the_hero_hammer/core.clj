16a495e5e3008596c87dbdd4dd127919e4ae9ae1
David <david@templatious.org>
2016-01-26 20:32:39 +0200

    theme

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -55,11 +55,16 @@
           [:link {:rel "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
                   :integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
                   :crossorigin "anonymous"}]
+          [:link {:rel "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+                  :integrity "sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+                  :crossorigin "anonymous"}]
           [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
                     :integrity "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
                     :crossorigin "anonymous"}]]
          [:body
-          towrap]
+          [:div {:class "container"}
+           [:div {:class "row"}
+            [:div {:class "col-md-7"} towrap]]]]
          ])
   )
 
