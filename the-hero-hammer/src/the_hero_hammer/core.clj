51a7002c47388fa129a342b4a3c8285123de0ec7
David <david@templatious.org>
2016-01-26 20:41:41 +0200

    MEREN

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -52,10 +52,12 @@
 (defn wrap-html [towrap]
   (html [:html
          [:head
-          [:link {:rel "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          [:link {:rel "stylesheet"
+                  :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
                   :integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
                   :crossorigin "anonymous"}]
-          [:link {:rel "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+          [:link {:rel "stylesheet"
+                  :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
                   :integrity "sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
                   :crossorigin "anonymous"}]
           [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
