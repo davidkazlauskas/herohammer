7f159b04643f99c1f4cdc7d270045a00e7c0db7a
David <david@templatious.org>
2016-01-31 09:18:12 +0200

    to vec

--- the-hero-hammer/src/the_hero_hammer/lol_context.clj
+++ the-hero-hammer/src/the_hero_hammer/lol_context.clj
@@ -511,7 +511,7 @@
 )
 
 (defn squares-for-heroes []
-  (map square-for-hero (all-heroes-lol)))
+  (into [] (map square-for-hero (all-heroes-lol))))
 
 (def ^:dynamic *all-questions-lol*
   (all-questions-lol))
