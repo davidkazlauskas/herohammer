999c024a856f0098399e757964cdd3a08052991d
David <david@templatious.org>
2016-02-02 19:06:08 +0200

    shared jerbs

--- the-hero-hammer/src/the_hero_hammer/shared_context.clj
+++ the-hero-hammer/src/the_hero_hammer/shared_context.clj
@@ -1,8 +1,18 @@
 (ns the-hero-hammer.shared_context
-  (:require [the-hero-hammer.questions :refer :all]
-            [the-hero-hammer.hh_context :refer :all]
-            [the-hero-hammer.hh_process :refer :all]
-            [the-hero-hammer.storage :as stor]
-            [taoensso.nippy :as nippy]
-            [co.paralleluniverse.pulsar.core :as pulsar]))
+  (:require [co.paralleluniverse.pulsar.core :as pulsar]))
+
+(defn process-questions [map-red-job]
+  (let [max-units 128]
+    (advance-map-reduce-job job max-units)))
+
+(defn schedule-question-processing [map-red-job]
+  (pulsar/spawn-fiber process-questions map-red-job))
+
+(defn main-job [map-red-job]
+  (while true
+    (schedule-question-processing map-red-job)
+    (pulsar/sleep (proc-delay-ms))))
+
+(defn gen-jobs [map-red-job]
+  (fn [] (pulsar/spawn-fiber main-job map-red-job)))
 
