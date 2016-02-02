846fc944e5e133d20d6f015a050a770415e2b092
David <david@templatious.org>
2016-02-02 19:08:29 +0200

    small refac

--- the-hero-hammer/src/the_hero_hammer/shared_context.clj
+++ the-hero-hammer/src/the_hero_hammer/shared_context.clj
@@ -3,15 +3,15 @@
 
 (defn process-questions [map-red-job]
   (let [max-units 128]
-    (advance-map-reduce-job job max-units)))
+    (advance-map-reduce-job map-red-job max-units)))
 
 (defn schedule-question-processing [map-red-job]
   (pulsar/spawn-fiber process-questions map-red-job))
 
-(defn main-job [map-red-job]
+(defn main-job [map-red-job proc-delay-ms]
   (while true
     (schedule-question-processing map-red-job)
-    (pulsar/sleep (proc-delay-ms))))
+    (pulsar/sleep proc-delay-ms)))
 
 (defn gen-jobs [map-red-job]
   (fn [] (pulsar/spawn-fiber main-job map-red-job)))
