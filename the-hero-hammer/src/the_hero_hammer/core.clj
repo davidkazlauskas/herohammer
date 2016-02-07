f38203d5f120cffe95ae5f5e8d3eca35e8ff264c
David <david@templatious.org>
2016-02-07 19:15:18 +0200

    the title

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -222,6 +222,7 @@
 (defn wrap-html [towrap]
   (html [:html
          [:head
+          [:title "The Hero Hammer - where whining becomes useful"]
           [:link {:rel "stylesheet"
                   :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
                   :integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
