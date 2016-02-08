46a740289c6ad1fe234f043845f5949669f74e4c
David <david@templatious.org>
2016-02-08 14:29:50 +0200

    hero data split

--- the-hero-hammer/src/the_hero_hammer/core.clj
+++ the-hero-hammer/src/the_hero_hammer/core.clj
@@ -684,6 +684,11 @@
       :opponent (Integer. (nth findings 2))}
       (Integer. (nth findings 3))]))
 
+(defn hero-data-split [the-str]
+  (let [findings (re-find #"(\d+)+-(\d+)" the-str)]
+    [{:hero (Integer. (nth findings 1))
+      :filter (Integer. (nth findings 2))}]))
+
 (defn render-answers [pairs]
   (let [questions (questions-full)]
     (html [:table {:class "table table-condensed"}
